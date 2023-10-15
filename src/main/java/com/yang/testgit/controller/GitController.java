e512df43378be18229767108ff204da94c4a8e5f
yang <1505256190@qq.com>
2023-10-15 15:59:51 +0800

    first commit

---
+++ src/main/java/com/yang/testgit/controller/GitController.java
@@ -0,0 +1,12 @@
+package com.yang.testgit.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitController {
+    public static void main(String[] args){
+        System.out.println("first");
+
+    }
+
+}
---
+++ src/test/java/com/yang/testgit/TestGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.testgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
